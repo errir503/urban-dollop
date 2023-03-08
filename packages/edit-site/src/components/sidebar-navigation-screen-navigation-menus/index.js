7babb50efd54c0ed95c400c72de50c4aadcd5402
errir <errir@outlook.jp>
2023-03-08 20:17:00 +0000

    Fix: Navigation sidebar shows a wrong submenu popover. (#48941) WordPress/gutenberg 667ab36cc239237fd2e716855c6ac6d7e99a9f0d

--- packages/edit-site/src/components/sidebar-navigation-screen-navigation-menus/index.js
+++ packages/edit-site/src/components/sidebar-navigation-screen-navigation-menus/index.js
@@ -5,7 +5,10 @@ import { __ } from '@wordpress/i18n';
 import { useCallback, useMemo } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
-import { BlockEditorProvider, Inserter } from '@wordpress/block-editor';
+import {
+	BlockEditorProvider,
+	privateApis as blockEditorPrivateApis,
+} from '@wordpress/block-editor';
 import { createBlock } from '@wordpress/blocks';
 
 /**
@@ -20,26 +23,30 @@ import { unlock } from '../../private-apis';
 import { store as editSiteStore } from '../../store';
 
 const noop = () => {};
-const NAVIGATION_MENUS_QUERY = { per_page: -1, status: 'publish' };
+const NAVIGATION_MENUS_QUERY = {
+	per_page: 1,
+	status: 'publish',
+	order: 'desc',
+	orderby: 'date',
+};
 
 function SidebarNavigationScreenWrapper( { children, actions } ) {
 	return (
 		<SidebarNavigationScreen
 			title={ __( 'Navigation' ) }
 			actions={ actions }
-			content={
-				<>
-					<p className="edit-site-sidebar-navigation-screen-navigation-menus__description">
-						{ __(
-							'Browse your site, edit pages, and manage your primary navigation menu.'
-						) }
-					</p>
-					{ children }
-				</>
-			}
+			description={ __(
+				'Browse your site, edit pages, and manage your primary navigation menu.'
+			) }
+			content={ children }
 		/>
 	);
 }
+
+const prioritizedInserterBlocks = [
+	'core/navigation-link/page',
+	'core/navigation-link',
+];
 
 export default function SidebarNavigationScreenNavigationMenus() {
 	const history = useHistory();
@@ -64,17 +71,7 @@ export default function SidebarNavigationScreenNavigationMenus() {
 			};
 		}, [] );
 
-	// Sort navigation menus by date.
-	const orderedNavigationMenus = useMemo(
-		() =>
-			navigationMenus?.sort( ( menuA, menuB ) => {
-				const menuADate = new Date( menuA.date );
-				const menuBDate = new Date( menuB.date );
-				return menuADate.getTime() > menuBDate.getTime();
-			} ),
-		[ navigationMenus ]
-	);
-	const firstNavigationMenu = orderedNavigationMenus?.[ 0 ]?.id;
+	const firstNavigationMenu = navigationMenus?.[ 0 ]?.id;
 	const blocks = useMemo( () => {
 		return [
 			createBlock( 'core/navigation', { ref: firstNavigationMenu } ),
@@ -107,6 +104,18 @@ export default function SidebarNavigationScreenNavigationMenus() {
 		},
 		[ history ]
 	);
+	const orderInitialBlockItems = useCallback( ( items ) => {
+		items.sort( ( { id: aName }, { id: bName } ) => {
+			// Sort block items according to `prioritizedInserterBlocks`.
+			let aIndex = prioritizedInserterBlocks.indexOf( aName );
+			let bIndex = prioritizedInserterBlocks.indexOf( bName );
+			// All other block items should come after that.
+			if ( aIndex < 0 ) aIndex = prioritizedInserterBlocks.length;
+			if ( bIndex < 0 ) bIndex = prioritizedInserterBlocks.length;
+			return aIndex - bIndex;
+		} );
+		return items;
+	}, [] );
 
 	if ( hasResolvedNavigationMenus && ! hasNavigationMenus ) {
 		return (
@@ -123,7 +132,7 @@ export default function SidebarNavigationScreenNavigationMenus() {
 			</SidebarNavigationScreenWrapper>
 		);
 	}
-
+	const { PrivateInserter } = unlock( blockEditorPrivateApis );
 	return (
 		<BlockEditorProvider
 			settings={ storedSettings }
@@ -133,7 +142,7 @@ export default function SidebarNavigationScreenNavigationMenus() {
 		>
 			<SidebarNavigationScreenWrapper
 				actions={
-					<Inserter
+					<PrivateInserter
 						rootClientId={ blocks[ 0 ].clientId }
 						position="bottom right"
 						isAppender
@@ -144,6 +153,7 @@ export default function SidebarNavigationScreenNavigationMenus() {
 							as: SidebarButton,
 							label: __( 'Add menu item' ),
 						} }
+						orderInitialBlockItems={ orderInitialBlockItems }
 					/>
 				}
 			>
